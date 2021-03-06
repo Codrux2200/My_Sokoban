aaad0e3f23dcbc28c90f4b68252e2b844e4ab5b9
Codrux2200 <saad.berrada@epitech.eu>
2021-03-06 22:46:44 +0100

    correct USAGE

--- lib/my/check_defeat.c
+++ lib/my/check_defeat.c
@@ -33,9 +33,9 @@ void print_describe(void)
     my_putstr("USAGE\n");
     my_putstr("    ./my_sokoban map\n");
     my_putstr("DESCRIPTION\n");
-    my_putstr("    map file representing the warehouse map, containing");
+    my_putstr("    map  file representing the warehouse map, containing");
     my_putstr(" ‘#’ for walls,\n");
-    my_putstr("        ‘P’ for the player, ‘X’ for boxes and");
+    my_putstr("         ‘P’ for the player, ‘X’ for boxes and");
     my_putstr(" ‘O’ for storage locations.\n");
 }
 
